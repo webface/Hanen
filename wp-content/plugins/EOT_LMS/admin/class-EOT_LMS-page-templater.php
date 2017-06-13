ac91f83dc953e407fa200dbfb7f0b9e2802a8e77
Hagai @ local <hagai@targetdirectories.com>
2017-06-13 13:50:27 -0400

    FIX: fixed the template issue with WP 4.7 +

--- wp-content/plugins/EOT_LMS/admin/class-EOT_LMS-page-templater.php
+++ wp-content/plugins/EOT_LMS/admin/class-EOT_LMS-page-templater.php
@@ -49,10 +49,16 @@ class PageTemplater {
 
 
                 // Add a filter to the attributes metabox to inject template into the cache.
-                add_filter(
-                                        'page_attributes_dropdown_pages_args',
-                                         array( $this, 'register_project_templates' ) 
-                                );
+                if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) { // 4.6 and older
+                    add_filter(
+                        'page_attributes_dropdown_pages_args',
+                        array( $this, 'register_project_templates' )
+                    );
+                } else { // Add a filter to the wp 4.7 version attributes metabox
+                    add_filter(
+                        'theme_page_templates', array( $this, 'add_new_template' )
+                    );
+                }
 
 
                 // Add a filter to the save post to inject out template into the page cache
@@ -117,6 +123,16 @@ class PageTemplater {
                 return $atts;
 
         } 
+
+        /**
+         * Adds our template to the page dropdown for v4.7+
+         *
+         */
+        
+        public function add_new_template( $posts_templates ) {
+           $posts_templates = array_merge( $posts_templates, $this->templates );
+           return $posts_templates;
+        }
 
         /**
          * Checks if the template is assigned to the page
