f2ade524cadb9436de28d1628415d2aef2a3059f
Tommy <tommy@targetdirectories.com>
2017-07-19 15:15:10 -0400

    FIX: indentation & formatting

--- wp-content/plugins/EOT_LMS/parts/part-view_quiz.php
+++ wp-content/plugins/EOT_LMS/parts/part-view_quiz.php
@@ -12,23 +12,30 @@
 <?php
 $true_subscription = verifyUserAccess();
 // Check if the subscription ID is valid.
-if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] != "") {
-    if (isset($true_subscription['status']) && $true_subscription['status']) {
-        if (current_user_can("is_director")) {
-            ?>
-            <?php
+if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] != "") 
+{
+    if (isset($true_subscription['status']) && $true_subscription['status']) 
+    {
+        if (current_user_can("is_director")) 
+        {
+            
             echo do_shortcode('[eot_quiz_admin action="view_quiz"]');
-            ?>
-            <?php
-        } else {
+            
+        } 
+        else 
+        {
             echo "ERROR: This subscription does not match your user's access permissions. Please contact the administrator at info@expertonlinetraining.com for help with this issue.";
             return;
         }
-    } else {
+    } 
+    else 
+    {
         echo "subscription ID does not belong to you";
         return;
     }
-} else {
+} 
+else 
+{
     echo "Could not find the subscription ID";
     return;
 }
