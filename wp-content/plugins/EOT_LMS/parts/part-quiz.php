707304fda609e04c165c48524eedf19ea8d33d98
Tommy <tommy@targetdirectories.com>
2017-08-23 16:27:24 -0400

    UPD: added verification to quizzes

--- wp-content/plugins/EOT_LMS/parts/part-quiz.php
+++ wp-content/plugins/EOT_LMS/parts/part-quiz.php
@@ -7,9 +7,10 @@
 </div>
 <h1 class="article_page_title">Take the quiz</h1>
 <?php
-	$quiz_id = isset($_REQUEST['quiz_id']) ? filter_var($_REQUEST['quiz_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
+
+    $quiz_id = isset($_REQUEST['quiz_id']) ? filter_var($_REQUEST['quiz_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
     $course_id = isset($_REQUEST['course_id']) ? filter_var($_REQUEST['course_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
-	$subscription_id = isset($_REQUEST['subscription_id']) ? filter_var($_REQUEST['subscription_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
+    $subscription_id = isset($_REQUEST['subscription_id']) ? filter_var($_REQUEST['subscription_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
     $enrollment_id = isset($_REQUEST['enrollment_id'])? filter_var($_REQUEST['enrollment_id'], FILTER_SANITIZE_NUMBER_INT) : 0;
 
 	// make sure we got a question ID, quiz ID, and subscription ID
@@ -19,14 +20,21 @@
 	//$true_subscription = verifyUserAccess();
 
 	if (verify_student_access($course_id)) 
-    {
+        {
+            if(verifyQuizInCourse($quiz_id, $course_id))
+            {
 		echo '<script>var quiz_id = ' . $quiz_id . '; var course_id = ' . $course_id . '; var enrollment_id = ' . $enrollment_id . ';</script>'; // set the quiz ID in JS
 
 		//load quiz
 		echo do_shortcode( '[eot_quiz_display action="view_quiz" id="' . $quiz_id . '"]' );
-    } 
-    else 
-    {
+            }
+            else
+            {
+                echo "This quiz is not part of your course";
+            }
+        } 
+        else 
+        {
         echo "subscription ID does not belong to you";
-    }
+        }
 ?>
