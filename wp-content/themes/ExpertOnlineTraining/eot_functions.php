9d81a0379ecae0f1ce5ec60ca3ae3a5d749218b9
Tommy <tommy@targetdirectories.com>
2017-08-25 10:45:27 -0400

    FIX: enrollment issue

--- wp-content/themes/ExpertOnlineTraining/eot_functions.php
+++ wp-content/themes/ExpertOnlineTraining/eot_functions.php
@@ -4132,17 +4132,26 @@ function enrollUserInCourses($courses = array(), $org_id = 0, $email = '', $subs
   $org_id = filter_var($org_id, FILTER_SANITIZE_NUMBER_INT);
   $subscription_id = filter_var($subscription_id, FILTER_SANITIZE_NUMBER_INT);
   $allcourses =  getCoursesById($org_id, $subscription_id);
+  $allcourses_names = array_column($allcourses, 'course_name');
   $result['message'] = ''; // will contain the success or failure messages and email statuses.
   $result['status'] = 1; // assume success unless we fail below
-
   // go through each course and enroll the user if course exists in all courses
+  
   foreach ($courses as $course_name)
   {
-    if($key = array_search($course_name, $allcourses))
+    if(in_array($course_name, $allcourses_names))// see if course exists in courses for this org
     {
-      $course_id = $courses[$key]->ID;  
-      $data = compact('org_id', 'course_name', 'course_id', 'subscription_id');
+      foreach($allcourses as $course)
+      {
+          if($course['course_name'] == $course_name)// get the matching course_id
+          {
+             $course_id = $course['ID'];  
+          }
+      }
       
+       
+      $data = compact('org_id', 'course_name', 'course_id', 'subscription_id');
+
       if(user_is_enrolled($email, $course_id))
       {
         $result['status'] = 0;
