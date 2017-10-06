3c373498651467b1a6ed18544bd2f51e4bcb308a
Adrian Baguio <adrian@targetdirectories.com>
2017-10-06 16:01:47 -0400

    UPD: Updated modules's category id.

---
+++ SQL/wp_modules_upgrade.sql
@@ -0,0 +1,15 @@
+UPDATE wp_modules
+SET CATEGORY_ID = 5
+WHERE title = "Preventing Gossip and Relational Aggression";
+
+UPDATE wp_modules
+SET CATEGORY_ID = 4
+WHERE title in ("Outdoor Cooking with Youth", "Equity & Diversity", "Programming For All (Differently Abled Youth)", "Effective Debriefing Tools and Techniques");
+
+UPDATE wp_modules
+SET CATEGORY_ID = 2
+WHERE title in ("Sexual Harassment","Fire Building and Fire Safety");
+
+UPDATE wp_modules
+SET CATEGORY_ID = 3
+WHERE title = "Cultural Competence in Youth Programs, Part I";
