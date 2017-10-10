6538b2d4330b2f58bf937236905e50be81dcb92f
Hagai @ local <hagai@targetdirectories.com>
2017-10-10 14:47:19 -0400

    UPD: added required fields for renewal script.

--- wp-content/plugins/EOT_LMS/parts/part-renewal_script.php
+++ wp-content/plugins/EOT_LMS/parts/part-renewal_script.php
@@ -1,4 +1,8 @@
-
+<style type="text/css">
+    table, caption, tbody, tfoot, thead, tr, th, td {
+        font-size: 95% !important;
+    }
+</style>
 <?php
 global $wpdb;
 // enqueue required javascripts
@@ -9,7 +13,7 @@ wp_enqueue_script('vfs-fonts', get_template_directory_uri() . '/js/vfs_fonts.js'
 wp_enqueue_script('buttons-html5', get_template_directory_uri() . '/js/buttons.html5.min.js', array(), '1.2.4', true);
 wp_enqueue_script('buttons-print', get_template_directory_uri() . '/js/buttons.print.min.js', array(), '1.2.4', true);
 $query = "
-        	SELECT u.ID as ID, u.display_name,u.user_email,uma.meta_value AS 'first_name', umb.meta_value AS 'last_name', 'manager' as role, s.trans_date, s.dash_price, s.id AS 'subscription_id'  
+        	SELECT u.ID as ID, u.display_name,u.user_email,uma.meta_value AS 'first_name', umb.meta_value AS 'last_name', 'manager' as role, s.trans_date, s.dash_price, s.id AS 'subscription_id', s.library_id   
         	FROM wp_users u 
         	LEFT JOIN wp_usermeta um ON u.ID = um.user_id
                 LEFT JOIN wp_subscriptions s ON u.ID = s.manager_id
@@ -34,9 +38,13 @@ $usersTableObj->headers = array(
     'Last_name' => 'center',
     'Date' => 'center',
     'Price' => 'center',
+    'Hash' => 'center',
+    'Library' => 'center',
     'Link' => 'center'
 );
+
 foreach ($managers as $manager) {
+    $hash = wp_hash($manager['user_email'].$manager['subscription_id']);
     //$subscription_id = getSubscriptionIdByUser($manager['ID']);
     $usersTableObj->rows[] = array(
         $manager['ID'],
@@ -46,8 +54,10 @@ foreach ($managers as $manager) {
         $manager['last_name'],
         $manager['trans_date'],
         $manager['dash_price'],
-        wp_hash($manager['user_email'].$manager['subscription_id'])
+        $hash,
+        $manager['library_id'],
+        '<a href="https://www.expertonlinetraining.com/renewal/?id='.$manager["ID"].'&key='.$hash.'">renew</a>'
     );
    // d($manager['user_email'].$manager['subscription_id']);
 }
-CreateDataTable($usersTableObj, "100%", 10, true, "Renewal List"); // Print the table in the page
+CreateDataTable($usersTableObj, "100%", 25, true, "Renewal List"); // Print the table in the page
