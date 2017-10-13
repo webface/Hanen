18a88babec50ed36875ba68e275d70e7a0d3be91
Adrian Baguio <adrian@targetdirectories.com>
2017-10-13 11:26:58 -0400

    CHG: Changed course resources page to display separately.

--- wp-content/plugins/EOT_LMS/parts/part-list_course_resources.php
+++ wp-content/plugins/EOT_LMS/parts/part-list_course_resources.php
@@ -9,43 +9,43 @@
 		<span class="current"> List Course Resources</span> 
 		<h1 class="article_page_title">List Course Resources</h1>
 <?php
-		// Tables for Course resources.
-		$courseResourcesTableObj = new stdClass();
-		$courseResourcesTableObj->rows = array();
-	  	$courseResourcesTableObj->headers = array(
-			'Course ID' => 'center',
-			'Course Title' => 'center',
-			'# Resources' => 'center',
-			'Resources' => '',
-		);
 		$courses = getCourses(0, 0, 0); // Default courses
 		foreach ($courses as $course) 
 		{
+			// Tables for Course resources.
+			$courseResourcesTableObj = new stdClass();
+			$courseResourcesTableObj->rows = array();
+		  	$courseResourcesTableObj->headers = array(
+		  		'Module ID' => 'center',
+				'Module Name' => 'center',
+				'# Resources' => 'center',
+				'Resources' => '',
+			);
 			$course_id = $course->ID; // Course ID
-			$types = array("exam", "video","doc", "link", "custom_video"); // Types of resources
 			$string = "<ul>";
-			$num_resources = 0; // Number of resources
-			foreach ($types as $type) 
+			$course_module = getCourseModules($course_id); // Coourse Modules
+			foreach ($course_module as $module) 
 			{
-				$resources = getResourcesInCourse($course_id, $type); // Resources in course
-				// Get all resources
+				$module = getModule($module['module_id']); // Module information
+				$resources = getResourcesInModule($module['ID']); // Module resources
+				$string = "<ul>";
 				foreach ($resources as $resource) 
 				{
-					$string .= "<li>" . $type . " - " . $resource['name'] . " - " . $resource['ID'];
-					$num_resources++;
+				
+					$string .= "<li>" . $resource['type'] . " - " . $resource['name'] . " - " . $resource['ID'];
 				}
+				$string .= "</ul>";
+				// Populate courseResourcesTableObj table
+		 		$courseResourcesTableObj->rows[] = array(
+		 			$module['ID'],
+		 			$module['title'],
+		 			count($resources),
+		 			$string
+				);
 			}
-			$string .= "</ul>";
-			// Populate courseResourcesTableObj table
-		 	$courseResourcesTableObj->rows[] = array(
-		    	$course_id,
-		    	$course->course_name,
-		    	$num_resources,
-		    	$string,
-			);	
-
+			echo '<h2>' . $course->course_name . '</h2>';
+			CreateDataTable($courseResourcesTableObj); // Print the table in the page	
 		}
-		CreateDataTable($courseResourcesTableObj); // Print the table in the page
 	}
 	else
 	{
