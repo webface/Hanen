2a1e376d41c1ec661e6cc0987cff1d8f4b54dd62
Adrian Baguio <adrian@targetdirectories.com>
2017-10-31 15:51:58 -0400

    ADD: Added SQL files to add helper videos tutorials.

---
+++ SQL/wp_help_topics_for_view_upgrade.sql
@@ -0,0 +1,112 @@
+/* Part Dashboard */
+INSERT INTO wp_help_topics_for_view (part_name, role, topic_id, `order`)
+VALUES
+("dashboard", 'manager', 21, 1),
+("dashboard", 'manager', 35, 2),
+
+/* Part View Content */
+("view_library", 'manager', 22, 1),
+("view_library", 'manager', 35, 2),
+
+/* Part Administration */
+("administration", 'manager', 21, 1),
+("administration", 'manager', 23, 2),
+("administration", 'manager', 24, 3),
+("administration", 'manager', 25, 4),
+("administration", 'manager', 26, 5),
+("administration", 'manager', 27, 6),
+("administration", 'manager', 30, 7),
+("administration", 'manager', 31, 8),
+("administration", 'manager', 32, 9),
+("administration", 'manager', 33, 10),
+("administration", 'manager', 34, 11),
+
+/* Part Manage Courses */
+("manage_courses", 'manager', 23, 1),
+("manage_courses", 'manager', 24, 2),
+("manage_courses", 'manager', 25, 3),
+("manage_courses", 'manager', 27, 4),
+("manage_courses", 'manager', 28, 5),
+("manage_courses", 'manager', 30, 6),
+
+/* Part Manage Staff Accounts */
+("manage_staff_accounts", 'manager', 26, 1),
+("manage_staff_accounts", 'manager', 27, 2),
+("manage_staff_accounts", 'manager', 29, 3),
+("manage_staff_accounts", 'manager', 30, 4),
+
+/* Part Invite Staff To Register */
+("invite_users", 'manager', 26, 1),
+("invite_users", 'manager', 27, 2),
+("invite_users", 'manager', 28, 3),
+("invite_users", 'manager', 29, 4),
+("invite_users", 'manager', 30, 5),
+("invite_users", 'manager', 31, 6),
+
+/* Part Upload Spreadsheet */
+("uploadspreadsheet", 'manager', 30, 1),
+("uploadspreadsheet", 'manager', 31, 2),
+
+/* Part Manage Your Custom Content */
+("upload_file", 'manager', 27, 1),
+("upload_file", 'manager', 31, 2),
+("upload_file", 'manager', 32, 3),
+
+/* Part Manage Your Custom Quizzes */
+("manage_quiz", 'manager', 27, 1),
+("manage_quiz", 'manager', 31, 2),
+("manage_quiz", 'manager', 33, 3),
+
+/* Part Manage Your Custom Modules */
+("manage_custom_modules", 'manager', 27, 1),
+("manage_custom_modules", 'manager', 31, 2),
+("manage_custom_modules", 'manager', 34, 3),
+
+/* Part Manage Your Email Staff */
+("improved_email_staff", 'manager', 31, 1),
+
+/* Part Customer Success */
+("customer_success", 'manager', 35, 1),
+
+/* Part Statistics */
+("statistics", 'manager', 27, 1),
+("statistics", 'manager', 28, 2),
+("statistics", 'manager', 31, 3),
+("statistics", 'manager', 30, 4),
+
+/* Part Course Statistics */
+("coursestats", 'manager', 28, 1),
+("coursestats", 'manager', 30, 2),
+("coursestats", 'manager', 31, 3),
+
+/* Part Staff Statistics */
+("coursestaffstats", 'manager', 28, 1),
+("coursestaffstats", 'manager', 30, 2),
+("coursestaffstats", 'manager', 31, 3),
+
+/* Part Directors Corner */
+("directors_corner", 'manager', 22, 1),
+("directors_corner", 'manager', 29, 2),
+("directors_corner", 'manager', 35, 3),
+
+/* Part Directors Professional Development */
+("directors_development", 'manager', 22, 1),
+("directors_development", 'manager', 29, 2),
+("directors_development", 'manager', 35, 3),
+
+/* Part Directors Bloopers */
+("bloopers", 'manager', 21, 1),
+("bloopers", 'manager', 22, 2),
+("bloopers", 'manager', 35, 3),
+
+/* Part Upgrade your Subscription / Add More Staff */
+("upgradesubscription", 'manager', 30, 1),
+("upgradesubscription", 'manager', 35, 2),
+
+/* Part Manage Logo */
+("manage_logo", 'manager', 35, 1),
+
+/* Part E-mail Your Staff */
+("email_staff", 'manager', 29, 1),
+("email_staff", 'manager', 30, 2),
+("email_staff", 'manager', 31, 3);
---
+++ SQL/wp_help_topics_upgrade.sql
@@ -0,0 +1,25 @@
+INSERT INTO wp_help_topics (title, summary, video_filename)
+VALUES
+('Tour the Director Dashboard', '', 'https://eot-videos.s3.amazonaws.com/tutorial_Tour_the_Dashboard.mp4'),
+('Watch Some Videos', 'This page allows you to view the content you have available for your staff members through your purchased subscription.', 'https://eot-videos.s3.amazonaws.com/tutorial_Watch_Some_Videos.mp4'),
+('Pick a Pre-Made Course', 'Use this feature to manage the default courses for your staff members.', 'https://eot-videos.s3.amazonaws.com/tutorial_Pick_a_Pre-Made_Course.mp4'),
+('Create a New Course', 'Use this feature to create you own custom course for your staff memebers.', 'https://eot-videos.s3.amazonaws.com/tutorial_Create_New_Course.mp4'),
+('Modify a Course', 'Use this feature to modify the default courses for your staff members.', 'https://eot-videos.s3.amazonaws.com/tutorial_Modify_a_Course.mp4'),
+('Create Staff Accounts', 'Use this feature to add staff and enroll them into courses. ', 'https://eot-videos.s3.amazonaws.com/tutorial_Create_Staff_Accounts.mp4'),
+('Enroll Staff in a Course', 'Use this feature to enroll you staff members into a course', 'https://eot-videos.s3.amazonaws.com/tutorial_Enroll_Staff_in_Course.mp4'),
+('Change Course Enrollment', 'Use this feature to modify user enrollments in specific courses.', 'https://eot-videos.s3.amazonaws.com/tutorial_Change_Course_Enrollment.mp4'),
+('Get Staff Started', '', 'https://eot-videos.s3.amazonaws.com/tutorial_Get_Staff_Started.mp4'),
+('Check Statistics', 'Use this feature to get statistics on how your staff is progressing through their assigned courses.', 'https://eot-videos.s3.amazonaws.com/tutorial_Check_Statistics.mp4'),
+('Email My Staff', '', 'https://eot-videos.s3.amazonaws.com/tutorial_Email_My_Staff.mp4'),
+('Upload Custom Content', 'Use this feature to upload your own custom content so that your staff has access to it.', 'https://eot-videos.s3.amazonaws.com/tutorial_Upload_Custom_Content.mp4'),
+('Write a Custom Quiz', 'Use this feature to create custom quizes for your staff.', 'https://eot-videos.s3.amazonaws.com/tutorial_Write_a_Custom_Quiz.mp4'),
+('Create a Custom Module', 'Use this feature to create custom modules for your staff consist of any number of resources.', 'https://eot-videos.s3.amazonaws.com/tutorial_Create_a_Custom_Module.mp4'),
+('Harness the Power of EOT', '', "https://eot-videos.s3.amazonaws.com/tutorial_Harness_EOT's_Power.mp4');
+
+
+
+
+
+
+
+
