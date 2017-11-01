9412e41143a318262ba95a9175466bd701ea1b80
Hagai @ local <hagai@targetdirectories.com>
2017-11-01 14:49:08 -0400

    FIX: fixed incorrect link on view library page

--- wp-content/plugins/EOT_LMS/parts/part-view_library.php
+++ wp-content/plugins/EOT_LMS/parts/part-view_library.php
@@ -24,7 +24,7 @@
           <br><br>
           <div class="center">
             <a href="<?=get_home_url()?>/premade-courses/" target="_blank"><button type="button" class="btn btn-primary">Pre-Made Courses</button></a>&nbsp;&nbsp;&nbsp;
-            <a href="<?=get_home_url()?>/choosing-a-topic/" target="_blank"><button type="button" class="btn btn-primary">Module Choices</button></a>&nbsp;&nbsp;&nbsp;
+            <a href="<?=get_home_url()?>/module-choices/" target="_blank"><button type="button" class="btn btn-primary">Module Choices</button></a>&nbsp;&nbsp;&nbsp;
             <a href="<?=get_home_url()?>/dashboard/?part=administration&subscription_id=<?=$subscription_id?>"><button type="button" class="btn btn-primary">Custom Content</button></a>
           </div>
           <br>
