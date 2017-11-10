ad561050ac9c0ff09b9cd22bc7a22a31aa5629e4
Hagai @ local <hagai@targetdirectories.com>
2017-11-10 11:33:32 -0500

    UPD: added language translation REM: removed percentage completed for individual courses so that it shows how many courses are complete. ie. if user1 finished 50% of 1 course and 0% of a second, it shows 0% completed, not 25%. When user1 completes the course, it will show 50% complete (1/2).

--- wp-content/plugins/EOT_LMS/parts/part-completedcourses.php
+++ wp-content/plugins/EOT_LMS/parts/part-completedcourses.php
@@ -29,15 +29,11 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 
 			if(count($learners) < 1)
 			{
-				?>
-					You do not have any staff accounts
-				<?php
+				echo __("You do not have any staff accounts", "EOT_LMS");
 			}
 			else if($learners == null)
 			{
-				?>
-					There's something wrong. Please contact the administrator.
-				<?php
+				echo __("There's something wrong. Please contact the administrator.", "EOT_LMS");
 			}
 			else
 			{
@@ -45,9 +41,9 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 				$quizTableObj->rows = array();
 		        $quizTableObj->headers = array(
 					'Name' => 'quiz-title',
-					'<div ' . hover_text_attr('The number of <b>Courses completed</b>.',true) .'>Courses Completed</div>' => 'center',
-					'<div ' . hover_text_attr('The <b>Status</b> of the user:<ul><li><b>Complete.</b> User has successfully passed all exams.</li><li><b>In Progress.</b> The user has logged in and started viewing videos but has not completed all of the exams yet.</li><li><b>Not Started.</b> The user has not logged in yet.</li></ul>',true) .'>Status</div>' => 'center',
-					'<div ' . hover_text_attr('This is a representation of the number of modules completed by the Staff member as a percentage of the total number of modules in the course.',true) .'>Progress</div>' => 'staff-progress'
+					'<div ' . hover_text_attr(__("The number of <b>Courses completed</b>.", "EOT_LMS"),true) .'>' . __("Courses Completed", "EOT_LMS") . '</div>' => 'center',
+					'<div ' . hover_text_attr(__("The <b>Status</b> of the user:<ul><li><b>Complete.</b> User has successfully passed all exams.</li><li><b>In Progress.</b> The user has logged in and started viewing videos but has not completed all of the exams yet.</li><li><b>Not Started.</b> The user has not logged in yet.</li></ul>", "EOT_LMS"), true) .'>' . __("Status", "EOT_LMS") . '</div>' => 'center',
+					'<div ' . hover_text_attr(__("This is a representation of the number of modules completed by the Staff member as a percentage of the total number of modules in the course.", "EOT_LMS"), true) .'>' . __("Progress", "EOT_LMS") . '</div>' => 'staff-progress'
 				);
 				$learners_user_info = array();
 				$enrollments = getEnrollments(0, 0, $org_id); // All enrollments in the organization.
@@ -65,7 +61,6 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 							$num_enrollments++;
 							$status = formatStatus($enrollment['status']); // The status of the enrollment
 							// Check if this enrollment is completed
-							$percentage_complete = $enrollment['percentage_complete'];
 							if($status == "Completed" || $status == "Passed")						
 							{
 								$num_complete_enrollment++;
@@ -74,15 +69,16 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 							else if($status == "Failed")
 							{
 								$status = 'In Progress';
-								$combined_percentage += $percentage_complete;
+								$combined_percentage += 0;
 							}
 							else 
 							{
-								$combined_percentage += $percentage_complete;
+								$combined_percentage += 0;
 							}
 						}
 					}
 					$overallpercent = $num_enrollments ? ($combined_percentage/($num_enrollments*100))*100 : 0; // The overall percentage for all courses
+					
 					// This determines if all the courses has been completed.
 					if($num_enrollments == $num_complete_enrollment)
 					{
@@ -93,34 +89,36 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 						// Count how many staff with incomplete course.
 						$num_staff_incomplete_course++;
 					}
+					
 					// make sure that status doesn't show not started if the user has some progress
 					if ($status == "Not Started" && $overallpercent > 0)
 					{
 						$status = "In Progress";
 					}
+					
 					$percentage = eotprogressbar('8em', $overallpercent, true);
-					$quizTableObj->rows[] = array($name, $num_complete_enrollment . " / " . $num_enrollments, ($num_enrollments == $num_complete_enrollment ? "Complete" : $status), $percentage);
+					$quizTableObj->rows[] = array($name, $num_complete_enrollment . " / " . $num_enrollments, ($num_enrollments == $num_complete_enrollment ? __("Complete", "EOT_LMS") : __("$status", "EOT_LMS")), $percentage);
 					$finished_user[] = $enrollment['email']; // Add the email to the finished user.
 				}
 			}
-		?>
+?>
 		<div class="breadcrumb">
 			<?= CRUMB_DASHBOARD ?>              
 			<?= CRUMB_SEPARATOR ?>  
 			<?= CRUMB_STATISTICS ?>  
 			<?= CRUMB_SEPARATOR ?>        
-		    <span class="current">Complete and Incomplete Statistics</span>   
+		    <span class="current"><?= __("Complete and Incomplete Statistics", "EOT_LMS") ?></span>   
 		</div>
 		<div class="smoothness">
-			<h1 class="article_page_title">Staff Statistics</h1>
-			Here are statistics on the staff who have <b>complete and incomplete courses.</b>
+			<h1 class="article_page_title"><?= __("Staff Statistics", "EOT_LMS") ?></h1>
+			<?= __("Here are statistics on the staff who have <b>complete and incomplete courses.</b>", "EOT_LMS") ?>
 
-			<h2>Summary</h2>
+			<h2><?= __("Summary", "EOT_LMS") ?></h2>
 
 			<div class="smoothness">
 				<div class="cell-row middle-row">
 				    <div class="cell-caption">
-				    	The total number of <b>Staff With Complete Courses</b>
+				    	<?= __("The total number of <b>Staff With Complete Courses", "EOT_LMS") ?></b>
 				    </div>
 				    <div class="cell-field number">
 				    	<b><?= $num_staff_completed_courses; ?></b>
@@ -128,15 +126,15 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
 				</div>
 				<div class="cell-row">
 					<div class="cell-caption">
-						The total number of <b>Staff With Incomplete Courses</b>
+						<?= __("The total number of <b>Staff With Incomplete Courses", "EOT_LMS") ?></b>
 					</div>
 					<div class="cell-field number">
 						<b><?= $num_staff_incomplete_course; ?></b>
 					</div>
 				</div>
 			</div>
-			<h2>Staff Statistics</h2>
-<?= 
+			<h2><?= __("Staff Statistics", "EOT_LMS") ?></h2>
+<?php 
 				CreateDataTable($quizTableObj, "100%", 25);
 ?>
 		</div>
@@ -144,16 +142,17 @@ if (isset($_REQUEST['subscription_id']) && $_REQUEST['subscription_id'] > 0)
         }
         else 
         {
-            echo "Unauthorized!";
+            echo __("Unauthorized!", "EOT_LMS");
         }
     }
     else 
     {
-        echo "subscription ID does not belong to you";
+        echo __("subscription ID does not belong to you", "EOT_LMS");
     }
 }
 // Could not find the subscription ID
 else 
 {
-	echo "Could not find the subscription ID";
+	echo __("Could not find the subscription ID", "EOT_LMS");
 }
+?>
