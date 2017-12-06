5972ff14a45dda6082e947c0cfb7f7aa0430510f
Tommy <tommy@targetdirectories.com>
2017-12-06 11:54:21 -0500

    UPD: added translations

--- wp-content/plugins/EOT_LMS/parts/part-uploadspreadsheet.php
+++ wp-content/plugins/EOT_LMS/parts/part-uploadspreadsheet.php
@@ -316,7 +316,7 @@
 						if($result)
 						{
 							//url to redirect to (redirects to a page where users are processed in instances of PENDING_USER_LIMIT users)
-							$url = get_home_url() .'/dashboard/?part=uploadspreadsheet&org_id=' . $org_id . '&subscription_id=' . $subscription_id . '&processing=1&max=' . count($staff_data);
+							$url = get_home_url() .'/dashboard/?part=uploadspreadsheet&org_id=' . $org_id . '&subscription_id=' . $subscription_id. '&user_id=' . $user_id . '&processing=1&max=' . count($staff_data);
 ?>
 							<!-- Redirecting (wp_redirect does not load html if headers have already been sent which is the case here so we have to use javascript)-->
 							<script type="text/javascript">
@@ -348,11 +348,11 @@
 		$subscription_id = filter_var($_REQUEST['subscription_id'],FILTER_SANITIZE_NUMBER_INT);	//subscription id
 		$admin_ajax_url = admin_url('admin-ajax.php');	
 ?>
-		<h1 class="article_page_title">Upload Staff Spreadsheet</h1>
+		<h1 class="article_page_title"><?= __('Upload Staff Spreadsheet','EOT_LMS')?></h1>
 
         <div class="spreadsheet_processing round_msgbox">
-			<strong>Please wait while we create your staff accounts: <br>
-            <span  class="processing">Processing <?= $processing ?> out of <?= $max ?> ...</span> </strong> <i class="fa fa-spinner fa-pulse fa-2x"></i><br /><br />DO NOT CLOSE THIS WINDOW UNTIL ALL STAFF ACCOUNTS HAVE BEEN CREATED.<br><br>You will be redirected to a success page once the import is complete.
+			<strong><?= __('Please wait while we create your staff accounts:','EOT_LMS')?> <br>
+            <span  class="processing">Processing <?= $processing ?> out of <?= $max ?> ...</span> </strong> <i class="fa fa-spinner fa-pulse fa-2x"></i><br /><br /><?= __('DO NOT CLOSE THIS WINDOW UNTIL ALL STAFF ACCOUNTS HAVE BEEN CREATED.','EOT_LMS')?><br><br><?= __('You will be redirected to a success page once the import is complete.','EOT_LMS')?>
 		</div>
             <div id="insert_form" style="display:none;"></div>
                 <script>
@@ -398,7 +398,7 @@
                                     }
                                     else if (count > <?= $max ?> && overall_status == 0)
                                     {
-                                        $('.round_msgbox').html("ERROR: Some accounts below did not get created.<br><br>Please contact us for assistance 1-877-390-2267 M-F 9-5 EST.<br><br>Error message is: " + result.message + "<br><br>" + sent_emails.replace(/,/g, "")); 
+                                        $('.round_msgbox').html("<?= __('ERROR: Some accounts below did not get created.','EOT_LMS')?><br><br><?= __('Please contact us for assistance 1-877-390-2267 M-F 9-5 EST.','EOT_LMS')?><br><br><?= __('Error message is:','EOT_LMS')?> " + result.message + "<br><br>" + sent_emails.replace(/,/g, "")); 
                                     }
                                     else
                                     {
@@ -420,44 +420,44 @@
 			wp_die('You do not have privilege for this subscription.');
 		}
 		?>
-		<h1 class="article_page_title">Upload Staff Spreadsheet</h1>
+		<h1 class="article_page_title"><?= __(, 'EOT_LMS')?>Upload Staff Spreadsheet</h1>
 		<p>
-      	You can <a href="<?= get_template_directory_uri() . '/templates/template-staff.xlsx' ?>">Download our Excel Template</a> and fill out your staff details, then upload it back to create your staff accounts. Make sure to follow these instructions carefully:
+      	You can <a href="<?= get_template_directory_uri() . '/templates/template-staff.xlsx' ?>"><?= __(, 'EOT_LMS')?>Download our Excel Template</a> <?= __(, 'EOT_LMS')?>and fill out your staff details, then upload it back to create your staff accounts. Make sure to follow these instructions carefully:
     	</p>
 		<ol>
 			<li>
-				<h2>Download the Excel Template</h2>
-				<a href="<?= get_template_directory_uri() . '/templates/template-staff.xlsx' ?>">Download the Excel Template (XLSX file)</a>. You will use this to fill in your staff data.
+				<h2><?= __(, 'EOT_LMS')?>Download the Excel Template</h2>
+				<a href="<?= get_template_directory_uri() . '/templates/template-staff.xlsx' ?>"><?= __(, 'EOT_LMS')?>Download the Excel Template (XLSX file)</a>. <?= __(, 'EOT_LMS')?>You will use this to fill in your staff data.
 				<br><br>
 			</li>
 			<li>
-		        <h2>Populate the Spreadsheet</h2>
-		        Enter your staff details using <b>Microsoft Excel</b> <i>(or a compatible application)</i> according to these guidelines:
+		        <h2><?= __(, 'EOT_LMS')?>Populate the Spreadsheet</h2>
+		        <?= __(, 'EOT_LMS')?>Enter your staff details using <b><?= __(, 'EOT_LMS')?>Microsoft Excel</b> <i><?= __(, 'EOT_LMS')?>(or a compatible application)</i> <?= __(, 'EOT_LMS')?>according to these guidelines:
 		        <div class="sm">
 			        <ul class="nested">
 			          	<li>
-			      			<b>First Name <span class="red">(required)</span></b>
+			      			<b><?= __(, 'EOT_LMS')?>First Name <span class="red"><?= __(, 'EOT_LMS')?>(required)</span></b>
 			          	</li>
 						<li>
-							<b>Last Name <span class="red">(required)</span></b>
+							<b><?= __(, 'EOT_LMS')?>Last Name <span class="red"><?= __(, 'EOT_LMS')?>(required)</span></b>
 						</li>
 						<li>
-							<b>E-mail <span class="red">(required)</span></b>
+							<b><?= __(, 'EOT_LMS')?>E-mail <span class="red"><?= __(, 'EOT_LMS')?>(required)</span></b>
 						</li>
 						<li>
-							<b>Password</b> - if left blank, a password will be generated (and will be included in a welcome e-mail automatically sent) <span class="red">If included, it must be a minimum of 6 characters.</span>
+							<b><?= __(, 'EOT_LMS')?>Password</b> <?= __(, 'EOT_LMS')?>- if left blank, a password will be generated (and will be included in a welcome e-mail automatically sent) <span class="red"><?= __(, 'EOT_LMS')?>If included, it must be a minimum of 6 characters.</span>
 						</li>
 						<li>
-				          	<b>Course <i>1, 2, 3, 4</i> <span class="red">(minimum 1 course)</span></b> - Enter the <b>Name</b> of the Course you want this staff member to join. It must <u>exactly match</u> the name of the Course, so it is recommended that you copy-and-paste the name to avoid errors.
+				          	<b><?= __(, 'EOT_LMS')?>Course <i><?= __(, 'EOT_LMS')?>1, 2, 3, 4</i> <span class="red"><?= __(, 'EOT_LMS')?>(minimum 1 course)</span></b><?= __(, 'EOT_LMS')?> - Enter the <b><?= __(, 'EOT_LMS')?>Name</b> <?= __(, 'EOT_LMS')?>of the Course you want this staff member to join. It must <u><?= __(, 'EOT_LMS')?>exactly match</u> <?= __(, 'EOT_LMS')?>the name of the Course, so it is recommended that you copy-and-paste the name to avoid errors.
 				            <ul class="nested">
 								<li>
-									You must put each user in <u>at least 1 Course</u> (in any of the 4 slots)
+									<?= __(, 'EOT_LMS')?>You must put each user in <u><?= __(, 'EOT_LMS')?>at least 1 Course</u> <?= __(, 'EOT_LMS')?>(in any of the 4 slots)
 								</li>
 								<li>
-									You can put each user into <u>up to 4 Courses</u> (there are 4 slots in the template)
+									<?= __(, 'EOT_LMS')?>You can put each user into <u><?= __(, 'EOT_LMS')?>up to 4 Courses</u> <?= __(, 'EOT_LMS')?>(there are 4 slots in the template)
 								</li>
 								<li>
-									You don't have to use the field <b>Course 1</b> (you can put a Course into <b>Course 2</b> and leave <b>Course 1</b> blank and it will still work fine. Our system is smart that way).
+									<?= __(, 'EOT_LMS')?>You don't have to use the field <b><?= __(, 'EOT_LMS')?>Course 1</b> <?= __(, 'EOT_LMS')?>(you can put a Course into <b><?= __(, 'EOT_LMS')?>Course 2</b> <?= __(, 'EOT_LMS')?>and leave <b><?= __(, 'EOT_LMS')?>Course 1</b> <?= __(, 'EOT_LMS')?>blank and it will still work fine. Our system is smart that way).
 								</li>
 				            </ul>
 			          	</li>
@@ -470,7 +470,7 @@
 								<div class="error-bl">
 									<div class="error-br">
 										<div class="errorbox">
-											<center>Do not delete, move, or add columns!</center>
+											<center><?= __(, 'EOT_LMS')?>Do not delete, move, or add columns!</center>
 										</div>              
 									</div>
 								</div>
@@ -482,8 +482,8 @@
 		        <br><br>
 	      	</li>
 	      	<li>
-		        <h2>Save as Tab-Delimited Text File</h2>
-		        	Once finished, click <b>Save As...</b> and save it as a <b>Tab-Delimited Text File</b> <img src="<?= get_template_directory_uri() ?>/images/info-sm.gif" title="<div class=\'sm\'><b>FYI:</b> This will actually make a text file version of the spreadsheet that will:<ul class=nested><li>not have any style formatting (ie: bold, italic, underline), and</li><li>each value will be separated by a tab</li></ul>This makes it easier for us to process the data.</div>" class="tooltip" style="margin-bottom: -2px" onmouseover="Tip('<div class=\'sm\'><b>FYI:</b> This will actually make a text file version of the spreadsheet that will:<ul class=nested><li>not have any style formatting (ie: bold, italic, underline), and</li><li>each value will be separated by a tab</li></ul>This makes it easier for us to process the data.</div>', FIX, [this, 45, -70], WIDTH, 240, DELAY, 5, FADEIN, 300, FADEOUT, 300, BGCOLOR, '#E5E9ED', BORDERCOLOR, '#A1B0C7', PADDING, 9, OPACITY, 90, SHADOW, true, SHADOWWIDTH, 5, SHADOWCOLOR, '#F1F3F5')" onmouseout="UnTip()"><br>
+		        <h2><?= __('Save as Tab-Delimited Text File', 'EOT_LMS')?></h2>
+		        	<?= __('Once finished, click', 'EOT_LMS')?> <b><?= __('Save As...', 'EOT_LMS')?></b> <?= __('and save it as a', 'EOT_LMS')?> <b><?= __('Tab-Delimited Text File', 'EOT_LMS')?></b> <img src="<?= get_template_directory_uri() ?>/images/info-sm.gif" title="<div class=\'sm\'><b>FYI:</b> <?= __('This will actually make a text file version of the spreadsheet that will: ', 'EOT_LMS')?><ul class=nested><li><?= __('not have any style formatting (ie: bold, italic, underline), and', 'EOT_LMS')?></li><li><?= __('each value will be separated by a tab', 'EOT_LMS')?></li></ul><?= __('This makes it easier for us to process the data.', 'EOT_LMS')?></div>" class="tooltip" style="margin-bottom: -2px" onmouseover="Tip('<div class=\'sm\'><b>FYI:</b> <?= __('This will actually make a text file version of the spreadsheet that will: ', 'EOT_LMS')?><ul class=nested><li><?= __('not have any style formatting (ie: bold, italic, underline), and', 'EOT_LMS')?></li><li><?= __('each value will be separated by a tab', 'EOT_LMS')?></li></ul><?= __('This makes it easier for us to process the data.', 'EOT_LMS')?></div>', FIX, [this, 45, -70], WIDTH, 240, DELAY, 5, FADEIN, 300, FADEOUT, 300, BGCOLOR, '#E5E9ED', BORDERCOLOR, '#A1B0C7', PADDING, 9, OPACITY, 90, SHADOW, true, SHADOWWIDTH, 5, SHADOWCOLOR, '#F1F3F5')" onmouseout="UnTip()"><br>
 		        <br>
 		        <br>
 		        <img src="<?= get_template_directory_uri() ?>/images/save-as-tab-delim.png" alt="Save as Tab-Delimited Text File">
@@ -495,7 +495,7 @@
 								<div class="error-bl">
 									<div class="error-br">
 										<div class="errorbox">
-											<center>NOTE: if you are using a mac and can not save as a tab delimited file, you may save as a comma seperated values (.CSV) file. Make sure you do not have any commas in any of the table cells!</center>
+											<center><?= __('NOTE: if you are using a mac and can not save as a tab delimited file, you may save as a comma seperated values (.CSV) file. Make sure you do not have any commas in any of the table cells!', 'EOT_LMS')?></center>
 										</div>              
 									</div>
 								</div>
@@ -505,21 +505,21 @@
 		      	</div>
 	      	</li>
 	      	<li>
-		        <h2>Upload the .TXT or .CSV file</h2>
+		        <h2><?= __('Upload the .TXT or .CSV file', 'EOT_LMS')?></h2>
 		    <?php
 				$options = array(
 						'post_id' => 'user_' . $user_id,
 						'field_groups' => array(ACF_UPLOAD_SPREADSHEET),
 						'return' => '?part=uploadspreadsheet&uploadFile=true&subscription_id='.$subscription_id.'&user_id='.$user_id,
-						'submit_value' => __("Upload Spreadsheet", 'acf'),
+						'submit_value' => __("Upload Spreadsheet", 'EOT_LMS'),
 				);
 			 	acf_form( $options ); 
 		    ?>
 		        <br>
 		        <div class="batch_upload round_msgbox">
-        			Please be patient while we create your user accounts ... <br />
+        			<?= __('Please be patient while we create your user accounts ... ', 'EOT_LMS')?><br />
         			<i class="fa fa-spinner fa-pulse fa-2x"></i> <br />
-        			If you see this message for more than 60 seconds, please call 877-390-2267 for assistance.  
+        			<?= __('If you see this message for more than 60 seconds, please call 877-390-2267 for assistance.', 'EOT_LMS')?>  
     			</div>
     			<script type="text/javascript">
     				jQuery(function($) {
@@ -533,18 +533,18 @@
 			    </script>
 		        <br>
 		        <div class="sm">
-			        <b>PLEASE NOTE</b>
+			        <b><?= __('PLEASE NOTE', 'EOT_LMS')?></b>
 			        <ol>
 						<li>
-							<b>Errors:</b><br>If there are any errors in the spreadsheet, it will give you a list of errors to fix. You must fix these errors, re-save as Tab-Delimited Text File, then upload the file again. No accounts will be created until the entire spreadsheet is valid.
+							<b><?= __('Errors:', 'EOT_LMS')?></b><br><?= __('If there are any errors in the spreadsheet, it will give you a list of errors to fix. You must fix these errors, re-save as Tab-Delimited Text File, then upload the file again. No accounts will be created until the entire spreadsheet is valid.', 'EOT_LMS')?>
 							<br><br>
 						</li>
 						<li>
-							<b>Existing Users:</b> <br>If a user already exists and you specify a <b>Course</b> to join (which they are not currently in), they will be added to that course. If the user exists and you <i>don't</i> specify any extra courses to join (that they are not in yet), it will be considered an erroneous duplicate, and you will need to remove this person from the spreadsheet.
+							<b><?= __('Existing Users:', 'EOT_LMS')?></b> <br><?= __('If a user already exists and you specify a', 'EOT_LMS')?> <b><?= __('Course', 'EOT_LMS')?></b><?= __('to join (which they are not currently in), they will be added to that course. If the user exists and you', 'EOT_LMS')?>  <i><?= __("don't", 'EOT_LMS')?></i> <?= __('specify any extra courses to join (that they are not in yet), it will be considered an erroneous duplicate, and you will need to remove this person from the spreadsheet.', 'EOT_LMS')?>
 								<br><br></li>
 						<li>
-							<b>Help!</b><br>
-							If you're having trouble, just give us a call at <b>877-390-2267</b> and we'll help you. You can even send your spreadsheet to us to get it checked.
+							<b><?= __('Help!', 'EOT_LMS')?></b><br>
+							<?= __("If you're having trouble, just give us a call at ", 'EOT_LMS')?><b><?= __('877-390-2267', 'EOT_LMS')?></b> <?= __("and we'll help you. You can even send your spreadsheet to us to get it checked.", 'EOT_LMS')?>
 
 						</li>
 			        </ol>
