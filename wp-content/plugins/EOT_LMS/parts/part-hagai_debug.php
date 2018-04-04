75f111dbc80ecee9c4ba3d2cf629abe6fa67854d
Hagai @ local <hagai@targetdirectories.com>
2018-04-04 13:12:56 -0400

    DBG: adding time trial debug.

--- wp-content/plugins/EOT_LMS/parts/part-hagai_debug.php
+++ wp-content/plugins/EOT_LMS/parts/part-hagai_debug.php
@@ -22,45 +22,6 @@ d($entries);
 		{
 			echo 'checking: ' . $entry[2] . '<br>';
 			$code = $entry[10];
-/*
-            $sql = 'SELECT * FROM ' . TABLE_INVITATIONS . ' WHERE code ="'.$code.'"';
-if ( SHOW_SQL ) error_log("hagai_debug: get_row-> $sql");
-            $current = $wpdb->get_row( $sql, ARRAY_A );//codes should be unique to user
-            $subscription_id = isset($current['subscription_id']) ? $current['subscription_id'] : '';
-			$user = get_user_by( 'email', $entry[2] );
-			$user_id = $user->ID;
-			$enrollments = getEnrollmentsByUserId($user_id, "all", $subscription_id);
-d($current, $enrollments);
-
-			$enrolled = FALSE;
-
-			// check if user is enrolled
-			if ( $enrollments )
-			{
-				// check if there is a specific course that the user is supposed to be in.
-				if ( isset( $current['course_id'] && !empty( $current['course_id'] ) )
-				{
-					foreach ( $enrollments as $enrollment )
-					{
-						// check that the user is enrolled in the same course
-						if ( $enrollment['course_id'] == $current['course_id'] )
-						{
-							$enrolled = TRUE;
-							break;
-						}
-					}
-				}
-			}
-			else 
-			{
-				// user is not enrolled, enroll him then send an email with appology.
-				// check if there is a specific course that the user is supposed to be in.
-				if ( isset( $current['course_id'] && !empty( $current['course_id'] ) )
-				{
-
-				}
-			}
-*/
 
             $sql = 'SELECT * FROM ' . TABLE_INVITATIONS . ' WHERE code ="'.$code.'"';
 if ( SHOW_SQL ) error_log("after_submission_register: get_row-> $sql");
@@ -251,6 +212,143 @@ echo 'added user to subscription : ' . $user->user_email . " with ID: $user_id t
             }
 		} // end foreach entry
 	} // end action invitation_code
+	else if ( $action = 'test_invitation' )
+	{
+		// message all the people who got an invitation code but are not enrolled in a course
+
+		// get all entries from registration form with camp invitation code.
+		$form_id = 3;
+		$search_criteria['field_filters'][] = array( 'key' => '10', 'operator' => 'isnot', 'value' => '' ); 
+		$entries = GFAPI::get_entries( $form_id, $search_criteria );
+d($entries);
+
+		// go through each user and check if they are enrolled in a course
+		foreach ( $entries as $entry )
+		{
+			echo 'checking: ' . $entry[2] . '<br>';
+			$code = $entry[10];
+
+            $sql = 'SELECT * FROM ' . TABLE_INVITATIONS . ' WHERE code ="'.$code.'"';
+if ( SHOW_SQL ) error_log("after_submission_register: get_row-> $sql");
+            $current = $wpdb->get_results( $sql, ARRAY_A ); //codes should be unique to user unless there is no course_id for the user
+ 
+            $num_rows = count($current);
+            $org_id = 0;
+            $subscription_id = 0;
+            $course_id = 0;
+            $type = '';
+            $invitation_id = 0;
+            $user_email = 0;
+
+            if ( $num_rows == 1 )
+            {
+error_log("current row: " . json_encode($current));
+            	// its a unique code so process accordingly
+	            $type = isset($current[0]['type']) ? $current[0]['type'] : '';
+	            $subscription_id = isset($current[0]['subscription_id']) ? $current[0]['subscription_id'] : '';
+	            $invitation_id = isset($current[0]['ID']) ? $current[0]['ID'] : '';
+	            $org_id = isset($current[0]['org_id']) ? $current[0]['org_id'] : '';
+	            $course_id = isset($current[0]['course_id']) ? $current[0]['course_id'] : 0;
+				$user_email = isset($current[0]['user_email']) ? $current[0]['user_email'] : $entry['2'];
+            }
+            else if ( $num_rows > 1 )
+            {
+            	// multiple possibilities so go through each code till we find the right user
+            	// type == user in this case but no course ID.
+            	foreach ( $current as $invitation )
+            	{
+            		// check the type of invitation it is.
+            		$type = isset($invitation['type']) ? $invitation['type'] : '';
+            		if ( $type == 'user' && $invitation['user_email'] == $entry['2'] )
+            		{
+			            $subscription_id = isset($invitation['subscription_id']) ? $invitation['subscription_id'] : '';
+			            $invitation_id = isset($invitation['ID']) ? $invitation['ID'] : '';
+			            $org_id = isset($invitation['org_id']) ? $invitation['org_id'] : '';
+			            $course_id = isset($invitation['course_id']) ? $invitation['course_id'] : 0;
+						$user_email = isset($invitation['user_email']) ? $invitation['user_email'] : $entry['2'];
+error_log("invitation: " . json_encode($invitation));
+            		}
+            		else if ( $type == 'org' || $type == 'course' )
+            		{
+			            $subscription_id = isset($invitation['subscription_id']) ? $invitation['subscription_id'] : '';
+			            $invitation_id = isset($invitation['ID']) ? $invitation['ID'] : '';
+			            $org_id = isset($invitation['org_id']) ? $invitation['org_id'] : '';
+			            $course_id = isset($invitation['course_id']) ? $invitation['course_id'] : 0;
+						$user_email = isset($invitation['user_email']) ? $invitation['user_email'] : $entry['2'];
+            		}
+            	}
+            }
+            else
+            {
+            	// code not found. We have a problem.
+            }
+
+echo "Invitation Data - type: $type, sub_id: $subscription_id, ID: $invitation_id, org: $org_id, course: $course_id, email: $user_email<br>";
+
+            // check if user is supposed to be enrolled in a course or just the org
+			if( $course_id )
+            {
+            	// make sure user in enrolled in course
+				$user = get_user_by( 'email', $entry[2] );
+				$user_id = $user->ID;
+				$enrollments = getEnrollmentsByUserId($user_id, "all", $subscription_id);
+
+				$enrolled = FALSE;
+
+				// check if user is enrolled
+				if ( $enrollments )
+				{
+					foreach ( $enrollments as $enrollment )
+					{
+						// check that the user is enrolled in the same course
+						if ( $enrollment['course_id'] == $course_id )
+						{
+							$enrolled = TRUE;
+							break;
+						}
+					}
+				}
+echo "Got course ID: $course_id checking if enrolled: $enrolled<br>";
+
+				if ( !$enrolled )
+				{
+					// enroll the user if org has enough staff accounts
+		            if( org_has_maxed_staff($org_id, $subscription_id) )
+		            { // org doesn't have enough staff credits, so put user in pending table
+echo "org has maxed its staff: adding to pending table<br><br>";
+		                $data=array(
+		                    'user_id' => $user_id,
+		                    'org_id' => $org_id,
+		                    'subscription_id' => $subscription_id,
+		                    'course_id' => $course_id,
+		                    'user_email' => $user_email
+		                );
+
+		            }
+		            else
+		            { // org has enough staff credits
+//		                update_user_meta($user_id, 'org_id', $org_id);
+echo "enrolling user ID: $user_id into course_id: $course_id and sending email<br><br>";
+	                    $course = getCourse($course_id);
+	                    $course_name = $course['course_name'];
+	                    $data = compact("org_id", "course_id", "course_name", "subscription_id");
+	                    
+		            }
+
+				}
+
+            }
+            else
+            {
+            	// no course id so just make sure user is in org/sub
+				$user = get_user_by( 'email', $entry[2] );
+				$user_id = $user->ID;
+//				add_user_in_subscription( $subscription_id, $user_id );
+//				update_user_meta($user_id, 'org_id', $org_id);
+echo 'added user to subscription : ' . $user->user_email . " with ID: $user_id to org: $org_id in sub: $subscription_id<br><br>";
+            }
+		} // end foreach entry
+	}
 	else
 	{
 		echo "Nothing to do here";
